17097198384f32199496aceb068ea871a188cd93
SonaBadalyan <sona.badalyan.1992@gmail.com>
2018-08-14 14:53:24 +0400

    fix

--- src/Base/RttSerializableTrait.php
+++ src/Base/RttSerializableTrait.php
@@ -17,7 +17,7 @@ trait RttSerializableTrait
 
            foreach ($vars as  $name => $value)
            {
-              $vars2[$name] = $className::getMemberName($name);
+              $vars2[$className::getMemberName($name)] = $value;
            }
            return $vars2;
        }
