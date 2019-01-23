83b37fa33e621a03a5ee6de94f359f04693f1f1b
SonaBadalyan <sona.badalyan.1992@gmail.com>
2019-01-23 15:37:12 +0400

    fix

--- src/Base/RttSerializableTrait.php
+++ src/Base/RttSerializableTrait.php
@@ -6,22 +6,19 @@ trait RttSerializableTrait
     public function jsonSerialize()
     {
         $vars = get_object_vars($this);
+
         $path = explode('\\', static::class);
         $className = array_pop($path);
         if (!$className) {
             throw new \Exception("Cannot find class in rtt list");
         }
         $vars2['rtt'] = array_search($className, Rtt::types);
+
         foreach ($vars as  $name => $value)
         {
             $member = (static::class)::getMemberName($name);
             if ($member['convertToDate']) {
                 $vars2[$member['key']] = date("Y-m-d H:i:s", $value);
-            } elseif ($member['isEnum'] != '') {
-                $str = $member['isEnum'];
-                $class = 'Class'.$str;
-                $EnumType = new $class();
-                $vars2[$member['key']] = EnumType.toString($value);
             } else {
                 $vars2[$member['key']] = $value;
             }
